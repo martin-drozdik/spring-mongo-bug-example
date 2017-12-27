5d453b54e06cae93eca3bb8d1400ec3021571eff
martin <drozdik.svk@gmail.com>
2017-12-27 17:27:05 +0100

    initial commit

---
+++ src/main/kotlin/com/example/demo/Service.kt
@@ -0,0 +1,32 @@
+package com.example.demo
+
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate
+import org.springframework.stereotype.Component
+import reactor.core.publisher.Mono
+
+data class Person (val id: String)
+
+@Component
+class PersonHandler(val template: ReactiveMongoTemplate)
+{
+    init
+    {
+        println("Initializing")
+
+        val jim: Mono<Person> =  template.save(Person("Jim"))
+        val john: Mono<Person> = template.save(Person("John"))
+        val jack: Mono<Person> = template.save(Person("Jack"))
+
+        launch(jim)
+        launch(john)
+        launch(jack)
+
+        println("Finished Initializing")
+    }
+
+    fun launch(mono: Mono<Person>)
+    {
+      //  mono.subscribe({println(it.id)}, {println("Error")}) // This works
+         mono.block()//  This just hangs
+    }
+}
